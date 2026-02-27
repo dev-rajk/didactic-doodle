ab7432b60d7239cda15a4f5ffa241eb138d6b43d
Devraj Kashyap <96662811+dev-rajk@users.noreply.github.com>
2026-02-27 23:28:25 +0530

    Update calendar_app.py

--- calendar_app.py
+++ calendar_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from db1 import add_event, get_events, approve_event, update_event, delete_event, decline_event
+from db import add_event, get_events, approve_event, update_event, delete_event, decline_event
 import pandas as pd
 import json
 
@@ -238,18 +238,11 @@ if choice == "View Events":
         # Display FullCalendar with modal functionality based on the global theme
         st.components.v1.html(fullcalendar(events, theme_mode=theme_mode), height=1024)
 
-        st.write('''
-                :red[**This website is free of cost for all users.** ] \n
-                :red[**The maintainers bear no responsibility for the accuracy of the event information, or any loss or damages resulting from the use of this site. The maintainer is not liable for any damages or misinformation.**] \n
-                :red[**Users are encouraged to verify all event information.**] \n
-                :red[**No personal infomation is tracked.**] \n
-                :green[**Cheers to quizzing.** ] :partying_face::partying_face:\n
-                  ''')
-
+    
             
     else:
         st.write("No approved events to show.")
-        st.write('''
+    st.write('''
                 :red[ **This website is free of cost for all users.** ] \n
                 :red[**The maintainers bear no responsibility for the accuracy of the event information, or any loss or damages resulting from the use of this site. The maintainer is not liable for any damages or misinformation.** ]  \n
                 :red[**Users are encouraged to verify all event information.**] \n
@@ -261,41 +254,38 @@ if choice == "View Events":
 # 2. Submitting Events
 elif choice == "Submit Event":
     st.subheader("Submit a New Quiz Event")
-    clearornot = st.checkbox(label="clear on exit")
-    with st.form(key='event_form', clear_on_submit=clearornot):
-        st.write(''':red[Please ensure that event details are accurate] ''') 
-        st.write(''':red[Fields marked with * are necessary]''')
-        Title = st.text_input("Quiz Name*")
-        Date = st.date_input("Date*")
-        Time = st.text_input("Time*")
-        Category = st.text_input("Category*")
-        Venue = st.text_input("Venue")  
-        Location = st.text_input("Location*")
-        Organizer = st.text_input("Organizer")
-        Genre = st.text_input("Genre")
-        Quiz_Master = st.text_input("Quiz Master")
-        Prize = st.text_input("Prize")
-        Contact = st.text_input("Contact Number*")
-        registration_link = st.text_input(" Registration Link")
-        other_details = st.text_area("Other Details")
-        
-        submit_button = st.form_submit_button(label='Submit Event')
-        
-        if submit_button:
-            if not Title or not Contact or not Location or not Date or not Category or not Time:
-                st.error("Please fill all the fields marked with a *")
-            else:
-                add_event(Title, Date, Time, Category, Venue, Location, Organizer, Genre, Quiz_Master, Prize, Contact, registration_link, other_details)
-                st.success(f"Event '{Title}' submitted successfully! Awaiting admin approval.")
-            
+    password = st.text_input("Enter Admin Password", type="password")
 
-    st.markdown('''
-                :red[ **This website is free of cost for all users.** ] \n
-                :red[**The maintainers bear no responsibility for the accuracy of the event information, or any loss or damages resulting from the use of this site. The maintainer is not liable for any damages or misinformation.** ]  \n
-                :red[**Users are encouraged to verify all event information.** ] \n
-                :red[**No personal infomation is tracked.** ] \n
-                :red[**Cheers to quizzing.** ] :partying_face::partying_face:\n
-                  ''')
+    if password == BASIC_ADMIN_PASSWORD:
+        clearornot = st.checkbox(label="clear on exit")
+        with st.form(key='event_form', clear_on_submit=clearornot):
+            st.write(''':red[Please ensure that event details are accurate] ''') 
+            st.write(''':red[Fields marked with * are necessary]''')
+            Title = st.text_input("Quiz Name*")
+            Date = st.date_input("Date*")
+            Time = st.text_input("Time*")
+            Category = st.text_input("Category*")
+            Venue = st.text_input("Venue")  
+            Location = st.text_input("Location*")
+            Organizer = st.text_input("Organizer")
+            Genre = st.text_input("Genre")
+            Quiz_Master = st.text_input("Quiz Master")
+            Prize = st.text_input("Prize")
+            Contact = st.text_input("Contact Number*")
+            registration_link = st.text_input(" Registration Link")
+            other_details = st.text_area("Other Details")
+            
+            submit_button = st.form_submit_button(label='Submit Event')
+            
+            if submit_button:
+                if not Title or not Contact or not Location or not Date or not Category or not Time:
+                    st.error("Please fill all the fields marked with a *")
+                else:
+                    add_event(Title, Date, Time, Category, Venue, Location, Organizer, Genre, Quiz_Master, Prize, Contact, registration_link, other_details)
+                    st.success(f"Event '{Title}' submitted successfully! Awaiting admin approval.")
+                
+
+
 
 
 # 3. Admin Panel for Approving, Editing, Deleting, and Exporting Events
